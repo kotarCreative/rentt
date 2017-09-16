44f4bac547e85a55f10c3f2572e0d6ee6e112385
Kotar Creative <kotarcreative@gmail.com>
2017-09-15 20:53:42 -0600

    Set up grid system. Finish up layout for home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,19 +13,61 @@
     <div class="row secondary">
         <div class="content">
             <div class="xs-1-1">
-                <h2>We know that finding a great location is only half the process.</h2>
+                <h2 class="sub-header">We know that finding a great location is only half the process.</h2>
             </div>
-            <div class="sm-1-2 xs-1-1">
+            <div class="sm-1-2 xs-1-1 sub-content-left">
                 <div class="tagline">
                     <h2>Tenants:</h2><h3 class="description">Read and write reviews about landlords</h3>
                 </div>
                 <p>Had a bad experience with a landlord?Wanna give kudos to a great landlord? Leave a review on their profile so that future tenants can be aware of what they may be getting themselves into. Read reviews left by past tenants to get a sense of what your prospective landlord is like.</p>
             </div>
-            <div class="sm-1-2 xs-1-1">
+            <div class="sm-1-2 xs-1-1 sub-content-right">
                 <div class="tagline">
                     <h2>Landlords:</h2><h3 class="description">Vet potential tenants</h3>
                 </div>
                 <p>View profiles of tenants to see their rental history, reviews left by previous landlords and LinkedIn profile. This allows you to determine which candidates you would like to book a viewing with and frees up your calendar from viewings with candidates that you know won't be a good fit.</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="content">
+            <div class="xs-1-1">
+                <h2 class="sub-header">Here's some ways we think we can make your rental experience better:</h2>
+            </div>
+            <div class="sm-1-2 xs-1-1">
+                <div class="tagline">
+                    <h2>Standardized Listing Info:</h2><h3 class="description">Search for what you care about</h3>
+                </div>
+                <p>Had a bad experience with a landlord?Wanna give kudos to a great landlord? Leave a review on their profile so that future tenants can be aware of what they may be getting themselves into. Read reviews left by past tenants to get a sense of what your prospective landlord is like.</p>
+            </div>
+            <div class="sm-1-2 xs-1-1">
+                <img>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="content">
+            <div class="sm-1-2 xs-1-1">
+                <img>
+            </div>
+            <div class="sm-1-2 xs-1-1">
+                <div class="tagline">
+                    <h2>Property Reviews:</h2><h3 class="description">See what other people think</h3>
+                </div>
+                <p>Had a bad experience with a landlord? Wanna give kudos to a great landlord? Leave a review on their profile so that future tenants can be aware of what they may be getting themselves into. Read reviews left by past tenants to get a sense of what your prospective landlord is like.</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="content">
+            <div class="sm-1-2 xs-1-1">
+                <div class="tagline">
+                    <h2>Connected Accounts:</h2><h3 class="description">Search for what you care about</h3>
+                </div>
+                <p>Had a bad experience with a landlord?Wanna give kudos to a great landlord? Leave a review on their profile so that future tenants can be aware of what they may be getting themselves into. Read reviews left by past tenants to get a sense of what your prospective landlord is like.</p>
+            </div>
+            <div class="sm-1-2 xs-1-1">
+                <img>
             </div>
         </div>
     </div>
