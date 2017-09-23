a66e4ee78a35ef512e0c04894b3be9cd2d1edcee
Kotar Creative <kotarcreative@gmail.com>
2017-09-22 23:30:48 -0600

    Fix class name on property typ migration.

--- database/migrations/2017_08_05_025816_create_property_type_table.php
+++ database/migrations/2017_08_05_025816_create_property_type_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDetailPropertyTable extends Migration
+class CreatePropertyTypeTable extends Migration
 {
     /**
      * Run the migrations.
