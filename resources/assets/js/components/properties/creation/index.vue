090fac2773c23908dbd67db66497f40102053abd
Kotar Creative <kotarcreative@gmail.com>
2017-09-22 23:38:02 -0600

    Add scaffolding for property creation.

---
+++ resources/assets/js/components/properties/creation/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="main-header">
+        <div class="logo-wrapper">
+            <img src="/imgs/main-logo.png" />
+        </div>
+        <ul class="creation-nav">
+            <li class="nav-item">
+                <button @click="changeSection('info')">Basic Info</button>
+            </li>
+            <li class="nav-item">
+                <button @click="changeSection('address')">Address</button>
+            </li>
+            <li class="nav-item">
+                <button @click="changeSection('Details')">Details</button>
+            </li>
+            <li class="nav-item">
+                <button @click="changeSection('Photos')">Photos</button>
+            </li>
+            <li class="nav-item">
+                <button @click="changeSection('Description')">Description</button>
+            </li>
+        </ul>
+    </div>
+</template>
