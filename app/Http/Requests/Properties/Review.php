fae4948a74914d9edd0f628acf42d8bbc5eb2353
Kotar Creative <kotarcreative@gmail.com>
2018-03-26 20:11:47 -0600

    Make basic profile info nullable.

--- app/Http/Requests/Properties/Review.php
+++ app/Http/Requests/Properties/Review.php
@@ -13,7 +13,7 @@ class Review extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return $this->user()->hasRole('tenant');
     }
 
     /**
--- database/migrations/2017_08_05_001400_create_users_table.php
+++ database/migrations/2017_08_05_001400_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('city_id');
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->unsignedInteger('city_id')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->string('password');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
