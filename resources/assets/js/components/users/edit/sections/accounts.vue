20f9f18c4529cf489175f0173c1d83e6db1b9dd6
David Buss <david.buss@pderas.com>
2018-03-31 16:02:51 -0600

    Add form for profile accounts editing.

--- resources/assets/js/components/users/edit/sections/accounts.vue
+++ resources/assets/js/components/users/edit/sections/accounts.vue
@@ -1,3 +1,50 @@
 <template>
-    <div></div>
+    <div id="profile-accounts-wrapper" class="creation-section-wrapper">
+        <h2>Connected Accounts</h2>
+        <div class="form-group">
+            <label for="linked-in-url">LinkedIn Profile Url<sup v-if="hasError('linked_in_url')" class="form-errors">*</sup></label>
+            <input
+                class="form-control"
+                type="text"
+                name="linked-in-url"
+                placeholder="www.linkedin.com"
+                v-model="user.linked_in_url"
+                @input="removeError('linked_in_url', $event)"
+            />
+        </div>
+        <div class="form-group">
+            <label for="airbnb-url">AirBnB Profile Url<sup v-if="hasError('airbnb_url')" class="form-errors">*</sup></label>
+            <input
+                class="form-control"
+                type="text"
+                name="airbnb-url"
+                placeholder="www.airbnb.com"
+                v-model="user.airbnb_url"
+                @input="removeError('airbnb_url', $event)"
+            />
+        </div>
+        <div class="form-errors" v-if="hasErrors()">
+            <sup>*</sup>Please Complete Required Fields
+        </div>
+    </div>
 </template>
+
+<script>
+    import ErrorMixins from '../../../../mixins/errorMixins';
+
+    export default {
+        name: 'profile-accounts-form',
+
+        mixins: [ ErrorMixins ],
+
+        data: () => ({
+            errorModel: 'users'
+        }),
+
+        computed: {
+            user() {
+                return this.$store.getters['users/active'];
+            }
+        },
+    }
+</script>
