fbed58ca28145d9d8724fc4b381ce9a59ccb22e4
Kotar Creative <kotarcreative@gmail.com>
2018-04-09 21:39:03 -0600

    Remove logo from emails to reduce spam.

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -6,20 +6,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
 
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div style="margin: 0px auto; max-width: 600px;">
-            <div style="padding: 15px 0px;">
-                <a href="{{ route('home') }}">
-                    <img class="rentt-logo" src="{{ url('/imgs/wordmark.png') }}" width="100px" alt="Rentt Logo">
-                </a>
-            </div>
             <div>
                 @yield('content')
                 <h4 style="font-size: 14px; font-weight: 500; font-family: 'Roboto'; margin-bottom: 6px; margin-top: 20px;">Thanks,</h4>
