7c05ce1b03440f7f0c8e36e89e9e915bce4623df
Kotar Creative <kotarcreative@gmail.com>
2018-06-06 21:35:39 -0600

    Add soft deletes to users and properties.

--- app/Models/Properties/Property.php
+++ app/Models/Properties/Property.php
@@ -3,9 +3,12 @@
 namespace App\Models\Properties;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Property extends Model
 {
+    use SoftDeletes;
+
     /**
      * The attributes that are mass assignable.
      *
