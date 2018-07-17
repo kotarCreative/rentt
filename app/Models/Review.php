6bce3d7603a00b5dcd95f5b1dfcd12a85b58c2af
Kotar Creative <kotarcreative@gmail.com>
2018-07-16 19:55:06 -0600

    Prevent comments from being duplicated. Resolves #73.

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -28,6 +28,7 @@ class Review extends Model
     {
         return $query->join('users', 'users.id', '=', 'reviews.reviewer_id')
             ->join('profile_pictures', 'profile_pictures.user_id', 'users.id')
+            ->where('profile_pictures.is_active', true)
             ->addSelect('users.first_name as reviewer_first_name')
             ->addSelect('profile_pictures.filepath as reviewer_profile_picture');
     }
