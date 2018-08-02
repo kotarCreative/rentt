2cb7c7ed797709970c8ab3c83577e241b01bac26
David Buss <david.buss@pderas.com>
2018-08-01 22:36:56 -0600

    Update copy on feedback page.

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -20,8 +20,8 @@
                 <h1 class="sub-header">Let us know what you think</h1>
             </div>
             <div class="xs-1-1">
-                <p>Here at Rentt we believe things can always be better (which is why we built Rentt). The only way that we can get better is if our users tell us how we can improve.</p>
-                <p>If there is something that Rentt does or doesn't do that you feel needs to change please let us know by filling out the form below. We are still a new company so if you come across anything that doesn't look right or isn't working we would love to hear about those issues to so that we can get them fixed for you.</p>
+                <p>Here at Rentt we believe things can always be better (which is why we built Rentt). The only way that we can improve is if you tell us how.</p>
+                <p>Is there something missing? Something broken? Something that needs to change? Fill out the form below so that we can get to work on it.</p>
             </div>
         </div>
     </div>
