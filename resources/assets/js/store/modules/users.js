ca30fb368be124e1539fb078d9faea77e044d5f7
Kotar Creative <kotarcreative@gmail.com>
2018-08-04 13:21:48 -0600

    Update redirect when profile update finished.

--- resources/assets/js/store/modules/users.js
+++ resources/assets/js/store/modules/users.js
@@ -141,7 +141,7 @@ const actions = {
         JSONToFormData(formData, user);
         axios.post('/profile', formData)
             .then(response => {
-                redirectTo('/profile?success=edit');
+                redirectTo('/profile/' + user.slug + '?success=edit');
                 dispatch('finishAjaxCall', {
                     loader: loader,
                     response: response,
