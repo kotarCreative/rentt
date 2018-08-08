ede9bc256fd7ed60cdd04e88463b6358fbbee5a2
Kotar Creative <kotarcreative@gmail.com>
2018-08-07 22:40:33 -0600

    show status of references and rental history

--- app/Models/Users/Reference.php
+++ app/Models/Users/Reference.php
@@ -7,15 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Reference extends Model
 {
     /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'denied_at'
-    ];
-
-    /**
      * The attributes that should be handled as dates.
      *
      * @var array
