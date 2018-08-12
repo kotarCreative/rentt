3e37abfdbd15f2f4b6adb1a78aa5cbf901692df4
Kotar Creative <kotarcreative@gmail.com>
2018-08-12 12:41:15 -0600

    Add google tag manager toggle to env file.

--- resources/views/layouts/creation.blade.php
+++ resources/views/layouts/creation.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    @if (env('USE_GOOGLE_TAG_MANAGER', false))
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -8,6 +9,7 @@
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-K8LGB8J');</script>
     <!-- End Google Tag Manager -->
+    @endif
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,10 +24,12 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    @if (env('USE_GOOGLE_TAG_MANAGER', false))
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8LGB8J"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
+    @endif
 
     <div id="app">
         @if (Auth::guest())
