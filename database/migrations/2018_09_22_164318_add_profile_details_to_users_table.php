715d2269366cec4ad12f33b40507935714b21cbd
Kotar Creative <kotarcreative@gmail.com>
2018-09-22 10:53:48 -0600

    Create migration for more user details.

---
+++ database/migrations/2018_09_22_164318_add_profile_details_to_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProfileDetailsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table) {
+          $table->unsignedInteger('age')
+            ->after('description')
+            ->nullable();
+          $table->enum('gender', [ 'Male', 'Female', 'Other' ])
+            ->nullable();
+          $table->enum('employment', [ 'Employed', 'Unemployed', 'Student' ])
+            ->after('linked_in_url')
+            ->nullable();
+          $table->boolean('is_searching_for_roommate')
+            ->after('is_a_smoker')
+            ->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table) {
+          $table->dropColumn([ 'employment', 'is_searching_for_roommate', 'gender', 'age' ]);
+        });
+    }
+}
